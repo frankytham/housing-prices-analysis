fd650a2ded89ba589d79b631b8ffc32d9441e1e0
Franky Tham <46061350+frankytham@users.noreply.github.com>
2022-01-09 00:46:50 +1100

    Setup React App

---
+++ app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Housing Prices Analysis</h1>
+        <p>Housing Prices Analysis</p>
+        <a
+          className="App-link"
+          href="https://github.com/frankytham/housing-prices-analysis"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
