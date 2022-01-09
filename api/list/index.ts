4771d7ee0f549f0aa445f63a222fdf25c6157e68
Franky Tham <46061350+frankytham@users.noreply.github.com>
2022-01-09 14:01:01 +1100

    Link React front end and Azure function

--- api/list/index.ts
+++ api/list/index.ts
@@ -1,15 +1,14 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log('HTTP trigger function processed a request.');
   const suburb = req.body && req.body.suburb;
-
   context.res = {
     status: 200,
     body: {
       suburb: suburb,
     }
   };
+  context.log(context.res);
 };
 
 export default httpTrigger;
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [suburb, setSuburb] = useState('');
+
+  const postSuburb = (event: any) => {
+    event.preventDefault();
+    fetch('/api/list', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        suburb: suburb
+      })
+    })
+    .catch(err => console.log('An error has occurred'));
+  };
+
+  const handleChange = (event: any) => {
+    setSuburb(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Housing Prices Analysis</h1>
-        <p>Housing Prices Analysis</p>
-        <a
-          className="App-link"
-          href="https://github.com/frankytham/housing-prices-analysis"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub Repo
-        </a>
+        <form id="suburb_form" className="App-form" onSubmit={postSuburb}>
+          <div>
+            <input 
+              type="text" 
+              id="suburb" 
+              className="App-input" 
+              placeholder="Suburb" 
+              value={suburb} 
+              onChange={handleChange} />
+            <button type="submit" className="App-button">Submit</button>
+          </div>
+        </form>
+        <a className="App-link" href="https://github.com/frankytham/housing-prices-analysis" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
       </header>
     </div>
   );
 }
-
-export default App;
