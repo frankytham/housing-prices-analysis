ca20c31f832ebd8d12adf400ce195b7f335a963e
Franky Tham <46061350+frankytham@users.noreply.github.com>
2022-01-10 23:51:50 +1100

    Update Azure function API

--- api/list/__init__.py
+++ api/list/__init__.py
@@ -1,15 +1,12 @@
 import azure.functions as func
-import logging
 
 def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
     try:
-        req_body = req.get_json()
-        suburb = req_body.get('suburb')
+        suburb = req.get_json().get('suburb')
     except ValueError:
         pass
 
-    logging.info(f"Request body: {req_body}")
     if suburb:
-        request_body = req.get_body()
-        doc.set(func.Document.from_json(request_body))
-        return func.HttpResponse(suburb, status_code=200)
+        req_body = req.get_body()
+        doc.set(func.Document.from_json(req_body))
+        return func.HttpResponse(req_body, status_code=200)
